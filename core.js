5ff44e187e5388b95c4339ee8c7388af3b56d1df
Jig356 <95321500+Jig356@users.noreply.github.com>
2022-04-07 11:28:11 -0400

    Update core.js

--- core.js
+++ core.js
@@ -107,7 +107,7 @@ terminal_animate('input')
 
 
 var socket;
-socket = io.connect('89.58.0.199:47185');
+socket = io.connect('v2202111161461170277.happysrv.de:47185');
 
 socket.on('input', foreignInput)
 
